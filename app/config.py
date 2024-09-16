a4254888ba5e46cacf4403b4c30cf40101bb5a03
Denis <n3m0_22@mail.ru>
2024-09-16 20:55:13 +0300

    Создание конфигурационного файла для текущего приложения и будущих сервисов

---
+++ app/config.py
@@ -0,0 +1,25 @@
+import os
+from sys import stdout
+from loguru import logger
+from dotenv import load_dotenv
+
+# Загрузка переменных из .env файла
+load_dotenv()
+
+# Получение имени текущей директории
+current_directory_name = os.path.basename(os.getcwd())
+
+# Формирование пути для лог-файлов
+log_file_path = os.path.join(
+    "..", "logs", f"{current_directory_name}_{{time:YYYY-MM-DD}}.log"
+)
+
+# Настройка ротации логов
+logger.add(
+    log_file_path,  # Файл лога будет называться по дате и сохраняться в поддиректории с названием текущей директории
+    rotation="00:00",  # Ротация каждый день в полночь
+    retention="7 days",  # Хранение логов за последние 7 дней
+    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}",  # Формат сообщений в файле
+    level="DEBUG",  # Минимальный уровень логирования
+    compression="zip",  # Архивирование старых логов
+)
